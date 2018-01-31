13e0e6fcf1749d264b6473069eff1e7d74f18d85
Chau <ctch5@mail.umsl.edu>
2018-01-30 21:31:55 -0600

    test

--- server-src/index.ts
+++ server-src/index.ts
@@ -41,7 +41,7 @@ function onServerError(error: NodeJS.ErrnoException): void {
 
 function onServerListening() {
     const addr = server.address();
-    console.log(addr);
+    console.log(process.env);
     const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`;
     winstonLogger.info(
         `-------------
