8876639edf593aa37df2d376aba0c6aa07fac910
Chau Tran <ctch5@mail.umsl.edu>
2018-07-26 21:00:07 -0500

    Fixed sync all entries for reporting

--- server-src/repositories/EntryRepository.ts
+++ server-src/repositories/EntryRepository.ts
@@ -10,16 +10,27 @@ export class EntryRepository extends BaseRepository<IEntry> implements IEntryRep
         this._entryModel = entryModel;
     }
 
-    public async findByQuery(farm?: string, recipient?: string): Promise<IEntry[]> {
+    public async findByQuery(crop?: string, recipient?: string, harvester?: string): Promise<IEntry[]> {
         let query;
-        if (!farm && recipient) {
+        if (!crop && recipient && !harvester) {
             query = {recipient};
-        } else if (!recipient && farm) {
-            query = {farm};
-        } else if (recipient && farm) {
-            query = {$and: [{recipient}, {farm}]};
+        } else if (!recipient && crop && !harvester) {
+            query = {crop};
+        } else if (!crop && !recipient && harvester) {
+            query = {harvester};
+        } else if (crop && recipient && !harvester) {
+            query = {$and: [{crop}, {recipient}]};
+        } else if (crop && !recipient && harvester) {
+            query = {$and: [{crop}, {harvester}]};
+        } else if (!crop && recipient && harvester) {
+            query = {$and: [{harvester}, {recipient}]};
+        } else if (recipient && crop && harvester) {
+            query = {$and: [{recipient}, {crop}, {harvester}]};
         }
-
         return await this._entryModel.find(query).exec();
+    }
+
+    public async updateMany(filter: any = {}, docs: any, options: any = {}) {
+        return this._entryModel.updateMany(filter, docs, options).exec();
     }
 }
